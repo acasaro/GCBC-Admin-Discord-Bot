c83e803b2a6881abc1fcffc7c4c6261ae2bafff2
acasaro <asaro913@gmail.com>
2023-09-29 10:07:40 -0700

    fixing config file

--- config.js
+++ config.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 const config = {
-  serverUrl: "Express Backend Host",
+  token: "Discord Application Token",
+  clientId: "Discord applications client id",
+  guildId: "Your development server's id",
 };
 
 if (process.env.NODE_ENV === "development") {
+  config.token = process.env.TEST_TOKEN;
+  config.clientId = process.env.TEST_CLIENT_ID;
+  config.guildId = process.env.TEST_GUILD_ID;
   config.serverUrl = process.env.SERVER_URL_DEV;
 } else {
+  config.token = process.env.TOKEN;
+  config.clientId = process.env.CLIENT_ID;
+  config.guildId = process.env.GUILD_ID;
   config.serverUrl = process.env.SERVER_URL_LIVE;
 }
 
